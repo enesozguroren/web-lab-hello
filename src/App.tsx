5dd30771096c4ed3325a6a7897904ceb738468e3
Enes Özgür Ören <enesozguroren@gmail.com>
2026-02-25 00:21:42 +0300

    chore: initial project setup with Vite React + TS

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div>
+      <h1>Web Tasarimi ve Programlama</h1>
+      <h2>LAB-1</h2>
+      <p>Ad Soyad: Enes Özgür Ören</p>
+      <p>Ogrenci No: 235541026</p>
+    </div>
+  )
+}
+
+export default App
+
